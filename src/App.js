55825be68f4b276836f0dbdf199e2983ade11e85
Kazi Minhaj <kaziminhajulhaider@gmail.com>
2021-03-22 06:14:43 +0600

    ema-john-simple-with-auth

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ function App() {
 
   return (
     <UserContext.Provider value={[loggedInUser, setLoggedInUser]} >
-      <h3> login: {loggedInUser.email}</h3>
       <Router>
         <Header />
         <Switch>
