44ffafee0756f572d83c83bdd8ec0d6321f11f49
Kazi Minhaj <kaziminhajulhaider@gmail.com>
2021-04-15 11:31:19 +0600

    50_4

--- src/components/Login/loginManager.js
+++ src/components/Login/loginManager.js
@@ -1,126 +1,152 @@
 import firebase from "firebase/app";
 import "firebase/auth";
-import firebaseConfig from './firebase.config';
+import firebaseConfig from "./firebase.config";
 
 export const initializedLoginFramework = () => {
-    if (!firebase.apps.length) {
-        firebase.initializeApp(firebaseConfig);
-    } else {
-        firebase.app(); // if already initialized, use that one
-    }
-}
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  } else {
+    firebase.app(); // if already initialized, use that one
+  }
+};
 
 export const handleGoogleSignIn = () => {
-    var googleProvider = new firebase.auth.GoogleAuthProvider();
-    return firebase.auth().signInWithPopup(googleProvider)
-        .then(result => {
-            const { displayName, photoURL, email } = result.user;
-            const signedUser = {
-                isSignedIn: true,
-                name: displayName,
-                email: email,
-                photo: photoURL,
-                success: true
-            }
-            return signedUser
-        })
-        .catch(error => {
-            console.log(error);
-        })
-}
+  var googleProvider = new firebase.auth.GoogleAuthProvider();
+  return firebase
+    .auth()
+    .signInWithPopup(googleProvider)
+    .then((result) => {
+      const { displayName, photoURL, email } = result.user;
+      const signedUser = {
+        isSignedIn: true,
+        name: displayName,
+        email: email,
+        photo: photoURL,
+        success: true,
+      };
+      setUserToken();
+      return signedUser;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const setUserToken = () => {
+  firebase
+    .auth()
+    .currentUser.getIdToken(/* forceRefresh */ true)
+    .then(function (idToken) {
+      sessionStorage.setItem("token", idToken);
+    })
+    .catch(function (error) {
+      // Handle error
+    });
+};
 
 export const handleFbSignIn = () => {
-    var fbProvider = new firebase.auth.FacebookAuthProvider();
-    return firebase
-        .auth()
-        .signInWithPopup(fbProvider)
-        .then((result) => {
-            console.log("facebook result:", result);
-            /** @type {firebase.auth.OAuthCredential} */
-            var credential = result.credential;
+  var fbProvider = new firebase.auth.FacebookAuthProvider();
+  return firebase
+    .auth()
+    .signInWithPopup(fbProvider)
+    .then((result) => {
+      console.log("facebook result:", result);
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
 
-            // The signed-in user info.
-            var user = result.user;
+      // The signed-in user info.
+      var user = result.user;
 
-            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-            var accessToken = credential.accessToken;
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      var accessToken = credential.accessToken;
 
-            // ...
-            user.success = true;
-            return user
-        })
-        .catch((error) => {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
+      // ...
+      user.success = true;
+      return user;
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
 
-            // ...
-            console.log("error code: ", errorCode, "error message :", errorMessage);
-        });
-}
+      // ...
+      console.log("error code: ", errorCode, "error message :", errorMessage);
+    });
+};
 
 export const handleSignOut = () => {
-    return firebase.auth().signOut().then(() => {
-        const signedOut = {
-            isSignedIn: false,
-            name: '',
-            email: '',
-            photo: ''
-        }
-        return signedOut
-    }).catch((error) => {
-        // An error happened.
+  return firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      const signedOut = {
+        isSignedIn: false,
+        name: "",
+        email: "",
+        photo: "",
+      };
+      return signedOut;
+    })
+    .catch((error) => {
+      // An error happened.
     });
-}
+};
 
 export const createUserWithEmailAndPassword = (email, password, name) => {
-    return firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then((response) => {
-            const newUserInfo = response.user;
-            newUserInfo.error = '';
-            newUserInfo.success = true;
-            updateUserName(name)
-            return newUserInfo
-        })
-        .catch((error) => {
-            const newUserInfo = {};
-            newUserInfo.error = error.message;
-            newUserInfo.success = false;
-            return newUserInfo
-        });
-}
+  return firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((response) => {
+      const newUserInfo = response.user;
+      newUserInfo.error = "";
+      newUserInfo.success = true;
+      updateUserName(name);
+      return newUserInfo;
+    })
+    .catch((error) => {
+      const newUserInfo = {};
+      newUserInfo.error = error.message;
+      newUserInfo.success = false;
+      return newUserInfo;
+    });
+};
 
 export const signInWithEmailAndPassword = (email, password) => {
-    return firebase.auth().signInWithEmailAndPassword(email, password)
-        .then((response) => {
-            const newUserInfo = response.user;
-            newUserInfo.error = '';
-            newUserInfo.success = true;
-            return newUserInfo
-        })
-        .catch((error) => {
-            const newUserInfo = {};
-            newUserInfo.error = error.message;
-            newUserInfo.success = false;
-            return newUserInfo
-        });
-}
+  return firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((response) => {
+      const newUserInfo = response.user;
+      newUserInfo.error = "";
+      newUserInfo.success = true;
+      return newUserInfo;
+    })
+    .catch((error) => {
+      const newUserInfo = {};
+      newUserInfo.error = error.message;
+      newUserInfo.success = false;
+      return newUserInfo;
+    });
+};
 
 const updateUserName = (name) => {
-    console.log("updateUserName", name);
-    var user = firebase.auth().currentUser;
+  console.log("updateUserName", name);
+  var user = firebase.auth().currentUser;
 
-    user.updateProfile({
-        displayName: name
-    }).then(function () {
-        // Update successful.
-        console.log('update successfully');
-    }).catch(function (error) {
-        // An error happened.
-        console.log(error);
+  user
+    .updateProfile({
+      displayName: name,
+    })
+    .then(function () {
+      // Update successful.
+      console.log("update successfully");
+    })
+    .catch(function (error) {
+      // An error happened.
+      console.log(error);
     });
-}
+};
--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,54 +1,81 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import { useForm } from "react-hook-form";
-import { UserContext } from '../../App';
-import { getDatabaseCart, processOrder } from '../../utilities/databaseManager';
-import './shipment.css';
+import { UserContext } from "../../App";
+import { getDatabaseCart, processOrder } from "../../utilities/databaseManager";
+import ProcessPayment from "../ProcessPayment/ProcessPayment";
+import "./shipment.css";
 
 const Shipment = () => {
-
-    const { register, handleSubmit, watch, errors } = useForm();
-    const onSubmit = data => {
-        const savedCart = getDatabaseCart()
-        const orderDetails ={...loggedInUser,products:savedCart,shipment: data, orderTime:new Date()}
-        fetch('https://whispering-spire-57909.herokuapp.com/addOrder',{
-            method:'POST',
-            headers:{
-                'Content-Type':'application/json'
-            },
-            body: JSON.stringify(orderDetails)
-        })
-        .then(res=>res.json())
-        .then(data => {
-            if(data){
-                processOrder()
-               alert('your order successfully') 
-            }
-        })
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = (data) => {
+    const savedCart = getDatabaseCart();
+    const orderDetails = {
+      ...loggedInUser,
+      products: savedCart,
+      shipment: data,
+      orderTime: new Date(),
     };
-    console.log(watch("example")); // watch input value by passing the name of it
+    fetch("https://whispering-spire-57909.herokuapp.com/addOrder", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(orderDetails),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data) {
+          processOrder();
+          alert("your order successfully");
+        }
+      });
+  };
+  console.log(watch("example")); // watch input value by passing the name of it
 
-    const [loggedInUser, setLoggedInUser] = useContext(UserContext)
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
 
-    return (
-        <form className="ship-form" onSubmit={handleSubmit(onSubmit)} >
+  return (
+    <div className="row">
+      <div className="col-md-6">
+        <form className="ship-form" onSubmit={handleSubmit(onSubmit)}>
+          <input
+            defaultValue={loggedInUser.name}
+            name="name"
+            ref={register({ required: true })}
+            placeholder="your name"
+          />
+          {errors.name && <span className="error">Name is required</span>}
 
-            < input defaultValue={loggedInUser.name} name="name" ref={register({ required: true })} placeholder="your name" />
-            { errors.name && <span className="error">Name is required</span>}
+          <input
+            defaultValue={loggedInUser.email}
+            name="email"
+            ref={register({ required: true })}
+            placeholder="your email"
+          />
+          {errors.email && <span className="error">Name is required</span>}
 
-            < input defaultValue={loggedInUser.email} name="email" ref={register({ required: true })} placeholder="your email" />
-            { errors.email && <span className="error">Name is required</span>}
+          <input
+            name="address"
+            ref={register({ required: true })}
+            placeholder="your address"
+          />
+          {errors.address && <span className="error">Name is required</span>}
 
-            < input name="address" ref={register({ required: true })} placeholder="your address" />
-            { errors.address && <span className="error">Name is required</span>}
+          <input
+            name="phone"
+            ref={register({ required: true })}
+            placeholder="your phone"
+          />
+          {errors.phone && <span className="error">Name is required</span>}
 
-            < input name="phone" ref={register({ required: true })} placeholder="your phone" />
-            { errors.phone && <span className="error">Name is required</span>}
-
-
-            <input type="submit" />
+          <input type="submit" />
         </form>
-    )
-
-}
+      </div>
+      <div className="col-md-6">
+        <ProcessPayment></ProcessPayment>
+      </div>
+    </div>
+  );
+};
 
 export default Shipment;
